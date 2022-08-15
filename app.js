00d455ce5cc7ca92c5336f48981897bf45d61669
Ebrar <16155052@mersin.edu.tr>
2022-08-15 12:04:14 +0300

    update-delete page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 const fileUpload = require('express-fileUpload');
 const ejs = require('ejs');
@@ -11,8 +12,9 @@ const app = express();
 
 //connect to DB
 mongoose.connect('mongodb://localhost:27017/pcat-test-db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
+  // useFindAndModify: false,
 });
 
 // TEMPLATE ENGINE
@@ -23,6 +25,10 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET']
+  }));
 
 // ROUTES
 app.get('/', async (req, res) => {
@@ -66,14 +72,42 @@ app.post('/photos', async (req, res) => {
   let uploadImage = req.files.image;
   let uploadPath = __dirname + '/public/uploads/' + uploadImage.name;
 
-  uploadImage.mv(uploadPath,async () => {
-      await Photo.create({
-        ...req.body,
-        image: '/uploads/' + uploadImage.name,
-      });
-      res.redirect('/');
+  uploadImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadImage.name,
     });
+    res.redirect('/');
+  });
 });
+
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+
+  res.render('edit', {
+    photo,
+  });
+});
+
+app.put('/photos/:id', async (req, res) => {
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.save();
+
+  res.redirect(`/photos/${req.params.id}`);
+});
+
+
+app.delete('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id })
+  let deletedImage = __dirname + '/public' + photo.image
+  fs.unlinkSync(deletedImage)
+  await Photo.findByIdAndRemove(req.params.id)
+  res.redirect('/')
+})
+
 
 const port = 3000;
 app.listen(port, () => {
